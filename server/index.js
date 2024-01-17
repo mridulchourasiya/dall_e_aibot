2ab60521872ef1e3cc56bdf11b10ba257fd8e6d7
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2024-01-17 15:58:44 +0530

    subcription is expired

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-
+import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
 const app = express();
@@ -9,12 +11,23 @@ app.use(cors())
 // Imports routes for each model (table) of the database
 app.use(express.json({limit: '50mb'}))
 
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+
 app.get('/', async (req, res) =>{
     res.send('hello world')
 })
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('server runnig on http://localhost:8080'))
+
+    try {
+        connectDB(process.env.MONGODB_URL)
+         app.listen(8080, () => console.log('server runnig on http://localhost:8080'))
+    } catch (error) {
+       console.log(error)   
+    }
+   
 }
 
 startServer()
